a880254bbc799263d6eccb263175d5869e4cb2e4
Bennet Krzenzck <bennetkrzenzck@t-online.de>
2025-05-02 23:42:49 +0200

    remove unused import

--- src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/Se2SpeedrunApplication.java
+++ src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/Se2SpeedrunApplication.java
@@ -2,7 +2,6 @@ package de.haw.se2.praktikum.speedrun.se2_speedrun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Se2SpeedrunApplication {
