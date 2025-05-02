892ce2754a5f21d716dc1076d44f2281d595b93d
Bennet Krzenzck <bennetkrzenzck@t-online.de>
2025-05-02 23:23:15 +0200

    add production and test environment

--- src/test/java/de/haw/se2/praktikum/speedrun/se2_speedrun/Se2SpeedrunApplicationTests.java
+++ src/test/java/de/haw/se2/praktikum/speedrun/se2_speedrun/Se2SpeedrunApplicationTests.java
@@ -2,7 +2,9 @@ package de.haw.se2.praktikum.speedrun.se2_speedrun;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Se2SpeedrunApplicationTests {
 
