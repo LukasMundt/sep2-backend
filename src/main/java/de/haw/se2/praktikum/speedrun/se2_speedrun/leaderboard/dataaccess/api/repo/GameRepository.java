2bf9028944527371067133a7cfb0989e3e199a98
Bennet <bennetkrzenzck@t-online.de>
2025-05-06 20:21:02 +0200

    wip implementing the getLeaderboard method

--- src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/dataaccess/api/repo/GameRepository.java
+++ src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/dataaccess/api/repo/GameRepository.java
@@ -1,7 +1,12 @@
 package de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.repo;
 
 import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.entity.Game;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GameRepository extends JpaRepository<Game, Long> {
+    Optional<Game> findByName(@NonNull @NotNull String name);
 }
--- src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
+++ src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
@@ -1,18 +1,33 @@
 package de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.facade.impl;
 
+import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.entity.Game;
+import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.repo.GameRepository;
+import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.repo.LeaderboardRepository;
 import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.facade.api.LeaderboardFacade;
 import de.haw.se2.praktikum.speedrun.se2_speedrun.openapitools.model.Leaderboard;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("${openapi.speedrunsOpenAPI30.base-path:}")
 public class LeaderboardFacadeImpl implements LeaderboardFacade {
 
+    private final GameRepository gameRepository;
+
+    @Autowired
+    public LeaderboardFacadeImpl(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
     @Override
     public ResponseEntity<Leaderboard> leaderboardGameCategoryGet(String game, String category) {
+        Optional<Game> leaderboardGame = gameRepository.findByName(game);
+        leaderboardGame.leaderboards()
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
