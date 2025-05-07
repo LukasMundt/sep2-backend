d2f8298a98eeca8131077a95f6d8bcc003061997
Bennet <bennetkrzenzck@t-online.de>
2025-05-07 13:30:39 +0200

    fixed missing speedrunner attribute in data model. Something is wrong with the mapper

--- src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/dataaccess/api/entity/LeaderboardEntry.java
+++ src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/dataaccess/api/entity/LeaderboardEntry.java
@@ -1,6 +1,7 @@
 package de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.entity;
 
 import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.common.api.datatype.Runtime;
+import de.haw.se2.praktikum.speedrun.se2_speedrun.user.dataaccess.api.entity.Speedrunner;
 import jakarta.validation.constraints.NotNull;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
@@ -28,4 +29,7 @@ public class LeaderboardEntry {
     @NonNull
     @NotNull
     private Runtime runtime;
+
+    @ManyToOne
+    private Speedrunner speedrunner;
 }
