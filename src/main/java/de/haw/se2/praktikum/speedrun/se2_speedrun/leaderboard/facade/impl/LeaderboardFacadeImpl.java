c318adbac0fa192c40012a8c1bb06fab8ff57da8
Bennet <bennetkrzenzck@t-online.de>
2025-05-07 07:28:41 +0200

    remvoed unused imports

--- src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
+++ src/main/java/de/haw/se2/praktikum/speedrun/se2_speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
@@ -1,8 +1,5 @@
 package de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.facade.impl;
 
-import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.entity.Game;
-import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.repo.GameRepository;
-import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.dataaccess.api.repo.LeaderboardRepository;
 import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.facade.api.LeaderboardFacade;
 import de.haw.se2.praktikum.speedrun.se2_speedrun.leaderboard.logic.api.usecase.LeaderboardUseCase;
 import de.haw.se2.praktikum.speedrun.se2_speedrun.openapitools.model.Leaderboard;
@@ -11,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("${openapi.speedrunsOpenAPI30.base-path:}")
