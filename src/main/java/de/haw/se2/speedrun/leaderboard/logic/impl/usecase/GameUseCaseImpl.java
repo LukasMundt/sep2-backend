8d57cb382935a7abcfa99d2497c44c1351671e3b
Bennet <bennetkrzenzck@t-online.de>
2025-05-12 14:25:21 +0200

    fixed some things

--- src/main/java/de/haw/se2/speedrun/leaderboard/logic/impl/usecase/GameUseCaseImpl.java
+++ src/main/java/de/haw/se2/speedrun/leaderboard/logic/impl/usecase/GameUseCaseImpl.java
@@ -4,9 +4,11 @@ import de.haw.se2.speedrun.leaderboard.dataaccess.api.entity.Game;
 import de.haw.se2.speedrun.leaderboard.dataaccess.api.repo.GameRepository;
 import de.haw.se2.speedrun.leaderboard.logic.api.usecase.GameUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class GameUseCaseImpl implements GameUseCase {
 
     private final GameRepository gameRepository;
