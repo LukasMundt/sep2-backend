5a795e5c02cf88354e7d7a981cf337025c9c6f75
Bennet <bennetkrzenzck@t-online.de>
2025-05-12 13:25:59 +0200

    find game by game slug

--- src/main/java/de/haw/se2/speedrun/leaderboard/dataaccess/api/repo/GameRepository.java
+++ src/main/java/de/haw/se2/speedrun/leaderboard/dataaccess/api/repo/GameRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    Optional<Game> findByName(@NonNull @NotNull String name);
+    Optional<Game> findBySlug(@NonNull @NotNull String gameSlug);
 }
