ce38b1bf5dd666ada6470b82e734d27d69271a8e
Bennet Krzenzck <bennetkrzenzck@t-online.de>
2025-05-13 12:00:04 +0200

    startet by implementing the auth facade

--- src/main/java/de/haw/se2/security/facade/impl/AuthenticationFacadeImpl.java
+++ src/main/java/de/haw/se2/security/facade/impl/AuthenticationFacadeImpl.java
@@ -1,6 +1,30 @@
 package de.haw.se2.security.facade.impl;
 
 import de.haw.se2.security.facade.api.AuthenticationFacade;
+import de.haw.se2.speedrun.openapitools.model.Credentials;
+import de.haw.se2.speedrun.openapitools.model.TokenResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("${openapi.speedrunsOpenAPI30.base-path:}")
 public class AuthenticationFacadeImpl implements AuthenticationFacade {
+
+    @Override
+    public ResponseEntity<Void> restAuthDelete() {
+        //https://docs.spring.io/spring-security/reference/servlet/authentication/logout.html#customizing-logout-uris
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseEntity<Void> restAuthGet(){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> restAuthPost(Credentials credentials) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
