91db4dc5798fd93b6f6ae434e95d2ba31d478fa1
Bennet Krzenzck <bennetkrzenzck@t-online.de>
2025-05-14 22:37:40 +0200

    SPRING SECURITY WORKS SÖLKDJFSOICUXHVBIJKEHSIOÖXJVCBLJKWGYLIXVJHBLJKHDEGSXKJVCHxkjshba<xicvjh 9821z hp3eij sha ifajkzhs dfpzuiasdzr 98231rhpfbsdkj,agzfv89z3 h jksz ciuaZ ER8IZU3Z UXC9PIOUFVZ389ẞP ZPSA<CUIVT Z39P8H< SCXF98V673Z

--- src/main/java/de/haw/se2/security/config/SecurityConfig.java
+++ src/main/java/de/haw/se2/security/config/SecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -111,6 +113,11 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
         JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
