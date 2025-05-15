f44c0d15e007873375a1d4406eb6c7f4dac5e14a
Bennet <bennetkrzenzck@t-online.de>
2025-05-15 14:57:31 +0200

    fix swagger ui try out feature

--- src/main/java/de/haw/se2/speedrun/Se2SpeedrunApplication.java
+++ src/main/java/de/haw/se2/speedrun/Se2SpeedrunApplication.java
@@ -1,9 +1,16 @@
 package de.haw.se2.speedrun;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://SE2-20.backend.dev.lukas-mundt.de")
+		}
+)
 @SpringBootApplication
 @ComponentScan(basePackages = {"de.haw.se2"})
 public class Se2SpeedrunApplication {
