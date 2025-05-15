555f4b215f0541fd09d23315f7e54723c6efddea
Bennet <bennetkrzenzck@t-online.de>
2025-05-15 11:44:02 +0200

    tried to fix cors error

--- src/main/java/de/haw/se2/speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
+++ src/main/java/de/haw/se2/speedrun/leaderboard/facade/impl/LeaderboardFacadeImpl.java
@@ -8,8 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin(
+        origins = {"http:localhost:5173", }, // allow only a specific domain
+        methods = {RequestMethod.GET, RequestMethod.POST}, // restrict methods
+        allowedHeaders = "*",
+        allowCredentials = "true"
+)
 @Controller
 @RequestMapping("${openapi.speedrunsOpenAPI30.base-path:}")
 public class LeaderboardFacadeImpl implements LeaderboardFacade {
